8aeffd893714dbe6edd917a76d2d82e827c5a3f3
DORNBACK <nathan.dornback@agilent.com>
2017-07-06 00:05:54 -0700

    Initial commit

---
+++ MicroDemo/main.c
@@ -0,0 +1,18 @@
+/*
+ * MicroDemo.c
+ *
+ * Created: 7/6/2017 12:03:38 AM
+ * Author : dornback
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
